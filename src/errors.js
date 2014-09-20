eb872526a1d0cbb42b92e4a7adfc406ca0c1a006
nathanfaucett <nathanfaucett@gmail.com>
2014-09-20 10:57:19 -0500

    updates

--- src/errors.js
+++ src/errors.js
@@ -1,9 +1,9 @@
 var errors = module.exports;
 
 
-errors.type = "invalid type";
+errors.type = "is of an invalid type";
 
-errors.empty = "not empty";
+errors.empty = "is not empty";
 
 errors.not_empty = errors.notempty = errors.notEmpty = "is empty";
 
@@ -13,53 +13,53 @@ errors["undefined"] = "is undefined";
 
 errors.object = "is not an object";
 
-errors.json = "invalid json";
+errors.json = "is invalid json";
 
-errors.text = "invalid text";
+errors.text = "is an invalid text";
 
-errors.string = "invalid string";
+errors.string = "is an invalid string";
 
-errors.alpha = "string contains non aplha characters";
+errors.alpha = "contains non aplha characters";
 
-errors.alpha_dashed = errors.alphadashed = errors.alphaDashed = "string contains non aplha dashed characters";
+errors.alpha_dashed = errors.alphadashed = errors.alphaDashed = "contains non aplha dashed characters";
 
-errors.numeric = "string contains non numeric characters";
+errors.numeric = "contains non numeric characters";
 
-errors.alpha_numeric = errors.alphanumeric = errors.alphaNumeric = "string contains non alpha numeric characters";
+errors.alpha_numeric = errors.alphanumeric = errors.alphaNumeric = "contains non alpha numeric characters";
 
-errors.alpha_numeric_dashed = errors.alphanumericdashed = errors.alphaNumericDashed = "string contains non alpha numeric dashed characters";
+errors.alpha_numeric_dashed = errors.alphanumericdashed = errors.alphaNumericDashed = "contains non alpha numeric dashed characters";
 
-errors.email = "invalid email";
+errors.email = "is an invalid email";
 
-errors.url = "invalid url";
+errors.url = "is an invalid url";
 
-errors.urlish = "invalid url";
+errors.urlish = "is an invalid url";
 
-errors.ip = "invalid ip";
+errors.ip = "is an invalid ip";
 
-errors.ipv4 = "invalid ip v4";
+errors.ipv4 = "is an invalid ip v4";
 
-errors.ipv6 = "invalid ip v6";
+errors.ipv6 = "is an invalid ip v6";
 
-errors.credit_card = errors.creditcard = errors.creditCard = "invalid credit card number";
+errors.credit_card = errors.creditcard = errors.creditCard = "is an invalid credit card number";
 
-errors.uuid = "invalid uuid";
+errors.uuid = "is an invalid uuid";
 
-errors.uuidv3 = "invalid uuid v3";
+errors.uuidv3 = "is an invalid uuid v3";
 
-errors.uuidv4 = "invalid uuid v4";
+errors.uuidv4 = "is an invalid uuid v4";
 
-errors["int"] = "not a integer";
+errors["int"] = "is not an integer";
 
-errors.integer = "not a integer";
+errors.integer = "is not an integer";
 
-errors.number = "not a number";
+errors.number = "is not a number";
 
 errors.finite = "is infinite";
 
-errors.decimal = "not a deciaml value";
+errors.decimal = "is not a deciaml value";
 
-errors["float"] = "not a deciaml value";
+errors["float"] = "is not a deciaml value";
 
 errors.falsey = "is not falsey";
 
@@ -69,51 +69,51 @@ errors["null"] = "is not null";
 
 errors.not_null = errors.notnull = errors.notNull = "is null";
 
-errors.bool = errors["boolean"] = "not boolean";
+errors.bool = errors["boolean"] = "is not boolean";
 
-errors.array = "not an array";
+errors.array = "is not an array";
 
-errors.binary = "not binary";
+errors.binary = "is not binary";
 
-errors.date = "not a date";
+errors.date = "is not a date";
 
-errors.date_time = errors.datetime = "not a date";
+errors.date_time = errors.datetime = "is not a date";
 
-errors.hexa_decimal = errors.hexadecimal = "invalid hexadecimal";
+errors.hexa_decimal = errors.hexadecimal = "is an invalid hexadecimal";
 
-errors.hex_color = errors.hexcolor = "invalid hex color";
+errors.hex_color = errors.hexcolor = "is an invalid hex color";
 
-errors.lowercase = "not lowercase";
+errors.lowercase = "is not lowercase";
 
-errors.uppercase = "not uppercase";
+errors.uppercase = "is not uppercase";
 
-errors.after = "not after";
+errors.after = "is not after";
 
-errors.before = "not before";
+errors.before = "is not before";
 
-errors.equals = "not equal";
+errors.equals = "is not equal";
 
 errors.contains = "does not contain";
 
 errors.not_contains = errors.notcontains = errors.notContains = "does contain";
 
-errors.length = errors.len = "invalid length";
+errors.length = errors.len = "is invalid length";
 
-errors["in"] = "not in";
+errors["in"] = "is ot in";
 
 errors.not_in = errors.notIn = "is in";
 
-errors.max = "greater than max";
+errors.max = "is greater than max";
 
-errors.min = "less than min";
+errors.min = "is less than min";
 
-errors.greater_than = errors.greaterthan = errors.greaterThan = "greater than min";
+errors.greater_than = errors.greaterthan = errors.greaterThan = "is greater than min";
 
-errors.less_than = errors.lessthan = errors.lessThan = "less than min";
+errors.less_than = errors.lessthan = errors.lessThan = "is less than min";
 
-errors.max_length = errors.maxlength = errors.maxLength = "invalid length";
+errors.max_length = errors.maxlength = errors.maxLength = "is invalid length";
 
-errors.min_length = errors.minlength = errors.minLength = "invalid length";
+errors.min_length = errors.minlength = errors.minLength = "is invalid length";
 
 errors.regex = "does not match";
 
